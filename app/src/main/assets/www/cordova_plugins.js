51b2ed66f45fe054a7b1f341839ec359a32ae39a
zhongyh <zhongyh6686@126.com>
2019-02-26 16:08:06 +0800

    增加audioinput插件用于录音，因为不是所有的浏览器支持录音。

--- app/src/main/assets/www/cordova_plugins.js
+++ app/src/main/assets/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-plugin-audioinput.AudioInput",
+    "file": "plugins/cordova-plugin-audioinput/www/audioInputCapture.js",
+    "pluginId": "cordova-plugin-audioinput",
+    "clobbers": [
+      "audioinput"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-audioinput": "1.0.1"
 };
 // BOTTOM OF METADATA
 });
